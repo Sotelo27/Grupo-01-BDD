5756a55eaf32823dd45d3ed72b4a8bed2cce87be
0marO <92110097+0marO@users.noreply.github.com>
2025-11-30 20:14:58 -0300

    agregada imagen extra. consultas requreidas

--- consultas-pedidas.sql
+++ consultas-pedidas.sql
@@ -39,14 +39,70 @@ VALUES (1, 'ana.perez@example.com');
 INSERT INTO publicaciones_textos_libres (id_publicacion, texto)
 VALUES (1, 'Ejemplo de texto libre');
 
+
 -- TODO: Realizar una publicación (tipo imagen).
+WITH p AS (
+INSERT INTO publicaciones (usuario_creador, id_grupo)
+VALUES ('ana.perez@example.com', NULL)
+    RETURNING id
+    )
+INSERT INTO publicaciones_imagenes (id_publicacion, contenido_imagen)
+SELECT id, pg_read_binary_file('/docker-entrypoint-initdb.d/imagen.jpg')
+FROM p;
+-- ahora un select para ver que se hizo bien
+SELECT * FROM publicaciones p
+JOIN publicaciones_imagenes pi ON p.id = pi.id_publicacion;
+
 -- TODO: Realizar una publicación (tipo video).
+
+    -- prar juli
+
 -- TODO: Actualizar una publicación (tipo texto).
+UPDATE publicaciones_textos_libres
+SET texto = 'Texto actualizado, mostrando un update'
+WHERE id_publicacion = 1;
+-- ahora un select para ver que se hizo bien
+SELECT * FROM publicaciones_textos_libres;
+
 -- TODO: Actualizar una publicación (tipo imagen).
+select * from publicaciones_imagenes;
+UPDATE publicaciones_imagenes
+SET contenido_imagen = pg_read_binary_file('/docker-entrypoint-initdb.d/imagen_new.jpg')
+WHERE id_publicacion = 2;
+
 -- TODO: Actualizar una publicación (tipo video).
+    -- para juli
 -- TODO: Eliminar una publicación (tipo texto).
+DELETE FROM publicaciones_textos_libres
+WHERE id_publicacion = 1;
+-- ahora un select para ver que se hizo bien
+SELECT * FROM publicaciones_textos_libres;
+
 -- TODO: Eliminar una publicación (tipo imagen).
+DELETE FROM publicaciones_imagenes
+WHERE id_publicacion = 2;
+
 -- TODO: Eliminar una publicación (tipo video).
+    -- para juli
+
 -- TODO: Desregistrar a un usuario de la aplicación (dar un ejemplo).
+DELETE FROM usuarios
+WHERE correo_electronico = 'lucia.martin@example.com';
+
 -- TODO: Mostrar las publicaciones más populares ordenadas por cantidad de “favoritos” que poseen.
+select * from publicaciones_favoritas;
+SELECT p.id id_publicacion , p.usuario_creador, COUNT(pf.correo_usuario) AS cantidad_favoritos
+FROM publicaciones p
+LEFT JOIN publicaciones_favoritas pf ON p.id = pf.id_publicacion
+GROUP BY p.id, p.usuario_creador
+ORDER BY cantidad_favoritos DESC;
+
+
+
 -- TODO: Mostrar los usuarios más populares basandose en la cantidad de publicaciones “favoritas” que poseen sus publicaciones.
+SELECT u.correo_electronico, u.nombre, u.apellido, COUNT(pf.correo_usuario) AS cantidad_favoritos
+FROM usuarios u
+LEFT JOIN publicaciones p ON u.correo_electronico = p.usuario_creador
+LEFT JOIN publicaciones_favoritas pf ON p.id = pf.id_publicacion
+GROUP BY u.correo_electronico, u.nombre, u.apellido
+ORDER BY cantidad_favoritos DESC;

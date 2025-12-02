8d263ced8bbf81c6eabb1e8e8402fb8a3013a04c
martin <lmsotelo@fi.uba.ar>
2025-12-02 11:57:59 -0300

    fix: corrijo tablas con ultima version

--- consultas-pedidas.sql
+++ consultas-pedidas.sql
@@ -33,14 +33,14 @@ FROM paises p
 LEFT JOIN usuarios u ON p.id_pais = u.id_pais
 GROUP BY p.nombre;
 
--- Realizar una publicación tipo texto libre -- TODO: capaz para las claves subrogadas (por ej IDs) sea mejor usar SERIAL (no del estándar pero si de postgres) en lugar de INT para no tener problemas con la repetición de IDs
+-- Realizar una publicación tipo texto libre
 INSERT INTO publicaciones (id_publicacion, correo_usuario_creador)
 VALUES (1, 'ana.perez@example.com');
 INSERT INTO publicaciones_textos_libres (id_publicacion, texto)
 VALUES (1, 'Ejemplo de texto libre');
 
 
--- TODO: Realizar una publicación (tipo imagen).
+-- Realizar una publicación (tipo imagen).
 WITH p AS (
 INSERT INTO publicaciones (usuario_creador, id_grupo)
 VALUES ('ana.perez@example.com', NULL)
@@ -49,54 +49,54 @@ VALUES ('ana.perez@example.com', NULL)
 INSERT INTO publicaciones_imagenes (id_publicacion, contenido_imagen)
 SELECT id, pg_read_binary_file('/docker-entrypoint-initdb.d/imagen.jpg')
 FROM p;
--- ahora un select para ver que se hizo bien
+-- Un select para ver que se hizo bien
 SELECT * FROM publicaciones p
 JOIN publicaciones_imagenes pi ON p.id_publicacion = pi.id_publicacion;
 
--- TODO: Realizar una publicación (tipo video).
+-- Realizar una publicación (tipo video).
 INSERT INTO publicaciones (id_publicacion, usuario_creador, id_grupo)
 VALUES (102, 'ana.perez@example.com', NULL);
 
 INSERT INTO publicaciones_videos (id_publicacion, contenido_video)
 VALUES (102, pg_read_binary_file('/docker-entrypoint-initdb.d/2025-11-17 18-13-52.mkv'));
 
--- TODO: Actualizar una publicación (tipo texto).
+-- Actualizar una publicación (tipo texto).
 UPDATE publicaciones_textos_libres
 SET texto = 'Texto actualizado, mostrando un update'
 WHERE id_publicacion = 1;
 -- ahora un select para ver que se hizo bien
 SELECT * FROM publicaciones_textos_libres;
 
--- TODO: Actualizar una publicación (tipo imagen).
+-- Actualizar una publicación (tipo imagen).
 select * from publicaciones_imagenes;
 UPDATE publicaciones_imagenes
 SET contenido_imagen = pg_read_binary_file('/docker-entrypoint-initdb.d/imagen_new.jpg')
 WHERE id_publicacion = 2;
 
--- TODO: Actualizar una publicación (tipo video).
+-- Actualizar una publicación (tipo video).
 UPDATE publicaciones_videos
 SET contenido_video = pg_read_binary_file('/docker-entrypoint-initdb.d/2025-11-17 18-13-52.mkv')
 WHERE id_publicacion = 102;
 
--- TODO: Eliminar una publicación (tipo texto).
+-- Eliminar una publicación (tipo texto).
 DELETE FROM publicaciones_textos_libres
 WHERE id_publicacion = 1;
--- ahora un select para ver que se hizo bien
+-- Un select para ver que se hizo bien
 SELECT * FROM publicaciones_textos_libres;
 
--- TODO: Eliminar una publicación (tipo imagen).
+-- Eliminar una publicación (tipo imagen).
 DELETE FROM publicaciones_imagenes
 WHERE id_publicacion = 2;
 
--- TODO: Eliminar una publicación (tipo video).
+-- Eliminar una publicación (tipo video).
 DELETE FROM publicaciones 
 WHERE id_publicacion = 102;
 
--- TODO: Desregistrar a un usuario de la aplicación (dar un ejemplo).
+-- Desregistrar a un usuario de la aplicación (dar un ejemplo).
 DELETE FROM usuarios
 WHERE correo_electronico = 'lucia.martin@example.com';
 
--- TODO: Mostrar las publicaciones más populares ordenadas por cantidad de “favoritos” que poseen.
+-- Mostrar las publicaciones más populares ordenadas por cantidad de “favoritos” que poseen.
 select * from publicaciones_favoritas;
 SELECT p.id_publicacion id_publicacion , p.usuario_creador, COUNT(pf.correo_usuario) AS cantidad_favoritos
 FROM publicaciones p
@@ -104,9 +104,7 @@ LEFT JOIN publicaciones_favoritas pf ON p.id_publicacion = pf.id_publicacion
 GROUP BY p.id_publicacion, p.usuario_creador
 ORDER BY cantidad_favoritos DESC;
 
-
-
--- TODO: Mostrar los usuarios más populares basandose en la cantidad de publicaciones “favoritas” que poseen sus publicaciones.
+-- Mostrar los usuarios más populares basandose en la cantidad de publicaciones “favoritas” que poseen sus publicaciones.
 SELECT u.correo_electronico, u.nombre, u.apellido, COUNT(pf.correo_usuario) AS cantidad_favoritos
 FROM usuarios u
 LEFT JOIN publicaciones p ON u.correo_electronico = p.usuario_creador
